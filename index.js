64dceed7dd507fee580fdac16fa60ec23916e0d4
Pragti Sengar <ps1606@dseu.ac.in>
2025-03-29 22:27:44 +0530

    first commit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from "express";
-import bodyParser from "body-parser"
-import pg from "pg"
+import bodyParser from "body-parser";
+import pg from "pg";
 
 const app = express();
 const port = 3000;
@@ -17,33 +17,38 @@ db.connect();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-let items = [
-  { id: 1, title: "wakup at 6 AM" },
-  { id: 2, title: "Study up to 8:30 AM" },
-];
+let items = [ ];
 
 app.get("/",async (req, res) => {
   try {
     const result=await db.query("SELECT * FROM items ORDER BY id ASC");
     items=result.rows;
     res.render("index.ejs",{
-      listTitle:"Task For Today",
+      listTitle:"List Your Daliy Targets",
       listItems:items,
     });
   }catch(err){
     console.log(err);
    }
   });
+  app.post("/add", async (req, res) => {
+    const { newItem, due_date, priority } = req.body;
+    if (!newItem) {
+      return res.status(400).send("Task title is required");
+    }
+    try {
+      await db.query(
+        "INSERT INTO items (title, due_date, priority) VALUES ($1, $2, $3)",
+        [newItem, due_date || null, priority || "Medium"]
+      );
+      res.redirect("/");
+    } catch (err) {
+      console.log("Error inserting due date:", err);
+    }
+  });
+  
 
-app.post("/add", async(req, res) => {
-  const item=req.body.newItem;
-  try {
-    await db.query("INSERT INTO items (title) VALUES($1)",[item]);
-    res.redirect("/");
-  }catch(err){
-    console.log(err);
-  }
-});
+
 
 app.post("/edit",async (req, res) => {
   const item=req.body.updatedItemTitle;
@@ -56,6 +61,20 @@ app.post("/edit",async (req, res) => {
     console.log(err);
   }
 });
+app.post("/update-status",async (req, res) => {
+  
+  const {status, id}=req.body;
+  
+
+  try {
+    await db.query("UPDATE items SET status = ($1) WHERE id = $2",[status,parseInt(id, 10)]);
+    res.redirect("/");
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+
 
 app.post("/delete", async(req, res) => {
   const id=req.body.deleteItemId;
@@ -71,3 +90,4 @@ app.post("/delete", async(req, res) => {
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
+ 
